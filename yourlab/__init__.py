91d75db71cfd18ba3779f8c730800f551ffc46c3
KDUser12 <lucas.29.deneffe@gmail.com>
2024-10-20 23:35:51 +0200

    Initial Branch

---
+++ yourlab/__init__.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python3
+
+""" YourLab Program
+
+YourLab simplifies the creation, backup, and management of Python projects,
+while offering various tools to automate common developer tasks. 
+
+"""
+
+__shortname__   = "YourLab"
+__longname__    = "YourLab : Personal Project Manager"
+__version__     = "1.0-release"
+
+forge_api_latest_release = "https://api.github.com/repos/kduser12/oshub/releases/latest"
